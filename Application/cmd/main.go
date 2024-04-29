0b5a07ec6254dab005a4ff7ca9c23775fa308864
Diyas Nurullayev <210103288@stu.sdu.edu.kz>
2024-04-29 20:56:29 +0500

    added config files

--- Application/cmd/main.go
+++ Application/cmd/main.go
@@ -5,16 +5,26 @@ import (
 	"Application/internal/handlers"
 	"Application/internal/repository"
 	"Application/internal/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handler := handlers.NewHandler(services)
 	srv := new(Application.Server)
-	err := srv.Run("8000", handler.InitRoutes())
+	err := srv.Run(viper.GetString("port"), handler.InitRoutes())
 	if err != nil {
 		log.Fatalf("error running server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
