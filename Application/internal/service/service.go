4b5ab22c710112e4c6025e1e842e581cee326327
MaxPrimal <99193464+MaxPrimal@users.noreply.github.com>
2024-05-07 22:12:32 +0500

    Update service.go

--- Application/internal/service/service.go
+++ Application/internal/service/service.go
@@ -13,6 +13,11 @@ type Administrator interface {
 }
 
 type Orders interface {
+	Create(product Application.Order, ctx *gin.Context) (string, error)
+	GetAll(ctx *gin.Context) ([]Application.Product, error)
+	GetById(id int, ctx *gin.Context) (Application.Order, error)
+	Delete(id int, ctx *gin.Context) (bool, error)
+	Update(order Application.Order, ctx *gin.Context) (int, error)
 }
 
 type Products interface {
